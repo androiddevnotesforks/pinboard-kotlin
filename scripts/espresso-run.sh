f34984198d4e5fdc4016bdf03c6947eb3fb7a89d
Filipe Belatti <fibelatti@gmail.com>
2020-11-17 19:08:17 +0000

    Update CI configuration

---
+++ scripts/espresso-run.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -eu
+
+cd ..
+
+echo "\n🔥 Install the apks...\n"
+find  . -name "*.apk" -print -exec adb install {} \;
+
+echo "\n🔥 Running instrumentation"
+
+EXECUTION=`adb shell am instrument -w com.fibelatti.pinboard.debug.test/androidx.test.runner.AndroidJUnitRunner`
+
+ERRORS_FOUND=`echo $EXECUTION | grep FAILURES | tr -d ' '`
+
+if [ -n "$ERRORS_FOUND" ]; then
+	echo "\n🔥 Instrumentation test execution failed!\n"
+	echo $EXECUTION
+	exit 1
+fi
+
+echo "\n🔥 Instrumentation tests ran with success!\n"
+exit 0
